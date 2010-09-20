255ebefded343f0b5492f74bc185dd0b50322970
Ross Singer <rossfsinger@gmail.com>
2010-09-20 12:09:52 -0400

    Adding gemspec, etc. for initial release

--- Rakefile.rb
+++ Rakefile.rb
@@ -11,3 +11,19 @@ Spec::Rake::SpecTask.new("specs") do |t|
   t.spec_files = FileList["specs/*.rb"]
 
 end
+
+begin
+  require 'jeweler'
+  Jeweler::Tasks.new do |gemspec|
+    gemspec.name = "frbr"
+    gemspec.summary = "A set of modules for creating simple FRBR relationships between objects."
+    gemspec.description = "A set of modules for creating simple FRBR (Functional Requirements for Bibliographic Resources) relationships between objects.  Includes Group 1, 2 and 3 entities."
+    gemspec.email = "rossfsinger@gmail.com"
+    gemspec.homepage = "http://github.com/rsinger/ruby-frbr"
+    gemspec.authors = ["Ross Singer"]
+    gemspec.files = Dir.glob("{lib,spec}/**/*") + ["README", "LICENSE"]
+    gemspec.require_path = 'lib'    
+  end
+rescue LoadError
+  puts "Jeweler not available. Install it with: sudo gem install technicalpickles-jeweler -s http://gems.github.com"
+end
